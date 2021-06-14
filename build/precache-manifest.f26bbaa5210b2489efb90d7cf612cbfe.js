da0305fb0428c255521798dbaddbf799be1ccd31
ayoubsmayen2 <ayoub.smayen@esprit.tn>
2021-06-15 00:06:34 +0200

    ert

---
+++ build/precache-manifest.f26bbaa5210b2489efb90d7cf612cbfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d01296807bd2c186ecb8",
+    "url": "/static/css/main.89cc34f1.chunk.css"
+  },
+  {
+    "revision": "d01296807bd2c186ecb8",
+    "url": "/static/js/main.d0129680.chunk.js"
+  },
+  {
+    "revision": "cdb16bde528589e77d34",
+    "url": "/static/js/1.cdb16bde.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e8d4dbc074108564cd87282c7cb780fe",
+    "url": "/index.html"
+  }
+];
